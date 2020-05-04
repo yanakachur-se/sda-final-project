e3a956dbbfbd57cdeed97e11b76253a8d9cf4570
Yingtong <ytdean@gmail.com>
2020-05-04 18:41:30 +0200

    update post function has been implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // Import custom styles for our application
-import "./App.css";
+import './App.css';
 
-import Auth from "./services/Auth";
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
+import Auth from './services/Auth';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 // Import pages
-import LoginPage from "./components/auth/LoginPage";
-import HomePage from "./components/home/HomePage";
-import PostsPage from "./components/posts/PostsPage";
+import LoginPage from './components/auth/LoginPage';
+import HomePage from './components/home/HomePage';
+import PostsPage from './components/posts/PostsPage';
 import ChatPage from './components/chat/ChatPage';
-import CommentsPage from './components/comments/CommentsPage'
-import ServicePost from "./components/posts/ServicePost"
+import CommentsPage from './components/comments/CommentsPage';
+import ServicePost from './components/posts/ServicePost';
 import ServicePage from './components/service/ServicePage';
 import ServiceForm from './components/service/serviceForm';
-
+import Covid19live from './components/covid19live/Covid19Live';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(Auth.isLoggedIn());
@@ -38,14 +38,17 @@ function App() {
             <ChatPage />
           </Route>
 
-          <Route exact path='/serviceform'>
-            <ServiceForm />
-          </Route>
-
           <Route path='/service'>
             <ServicePage />
           </Route>
-          
+
+          <Route path='/serviceform'>
+            <ServiceForm />
+          </Route>
+
+          <Route path='/covid19live'>
+            <Covid19live />
+          </Route>
           <Route path='/'>
             <HomePage />
           </Route>
