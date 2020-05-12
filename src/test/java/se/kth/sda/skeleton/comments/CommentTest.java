8bf84447a23d06157f61bf7e9e14bd5208cdb208
Maria Gubolin <mlgubolin@gmail.com>
2020-05-12 23:57:30 +0200

    getId and getAll tests working

--- src/test/java/se/kth/sda/skeleton/comments/CommentTest.java
+++ src/test/java/se/kth/sda/skeleton/comments/CommentTest.java
@@ -2,74 +2,87 @@ package se.kth.sda.skeleton.comments;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import se.kth.sda.skeleton.auth.AuthService;
+import se.kth.sda.skeleton.posts.Post;
 import se.kth.sda.skeleton.user.User;
 import se.kth.sda.skeleton.user.UserService;
 
+import java.util.Optional;
+
 @SpringBootTest
-class ControllerTests {
-//    @Autowired
-//    TestRestTemplate testRestTemplate;
-//
-//    @Autowired
-//    AuthService authService;
-//
-//    @Autowired
-//    UserService userService;
-//
-//    private String getAuthHeader() {
-//        userService.register(new User("test@exmaple.com", "pass123123", "John"));
-//        return "Bearer " + authService.createAuthToken("test@exmaple.com");
-//    }
-//
-//    @Test
-//    public void getAll() {
-//        // Arrange
-//        // Add tasks
-//
-//        // Act
-//        // Send an http request to /tasks
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add(HttpHeaders.AUTHORIZATION, getAuthHeader());
-//        HttpEntity<String> request = new HttpEntity<>("", headers);
-//
-//        String body = testRestTemplate.exchange(
-//                "/tasks",
-//                HttpMethod.GET,
-//                request,
-//                String.class
-//        ).getBody();
-//
-//        // Assert
-//        Assertions.assertEquals("[]", body);
-//    }
-//
-//    @Test
-//    public void getAll1() {
-//        // Arrange
-//        // Add tasks
-//
-//        // Act
-//        // Send an http request to /tasks
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add(HttpHeaders.AUTHORIZATION, getAuthHeader());
-//        HttpEntity<String> request = new HttpEntity<>("", headers);
-//
-//        String body = testRestTemplate.exchange(
-//                "/tasks",
-//                HttpMethod.GET,
-//                request,
-//                String.class
-//        ).getBody();
-//
-//        // Assert
-//        Assertions.assertEquals("[]", body);
-//    }
+class CommentTests {
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Autowired
+    AuthService authService;
+
+    @MockBean
+    CommentService commentService;
+
+    User userMock = new User();
+    Post postMock = new Post();
+
+
+
+    Comment comment = new Comment(1L,"aaa",postMock,userMock);
+
+    private String getAuthHeader() {
+        commentService.create(comment);
+        return "Bearer " + authService.createAuthToken("test@exmaple.com");
+    }
+
+    @Test
+    public void getAll() {
+        // Arrange
+        // Add tasks
+        Mockito.when(commentService.create(Mockito.eq(comment)))
+                .thenReturn(comment);
+        // Act
+        // Send an http request to /tasks
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, getAuthHeader());
+        HttpEntity<String> request = new HttpEntity<>("", headers);
+
+        String body = testRestTemplate.exchange(
+                "/tasks",
+                HttpMethod.GET,
+                request,
+                String.class
+        ).getBody();
+
+        // Assert
+        Assertions.assertEquals("[]", body);
+    }
+
+    @Test
+    public void getAll1() {
+        // Arrange
+        // Add tasks
+
+        // Act
+        // Send an http request to /tasks
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, getAuthHeader());
+        HttpEntity<String> request = new HttpEntity<>("", headers);
+
+        String body = testRestTemplate.exchange(
+                "/tasks",
+                HttpMethod.GET,
+                request,
+                String.class
+        ).getBody();
+
+        // Assert
+        Assertions.assertEquals("[]", body);
+    }
 
 }
--- src/test/java/se/kth/sda/skeleton/posts/PostTests.java
+++ src/test/java/se/kth/sda/skeleton/posts/PostTests.java
@@ -6,16 +6,32 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import se.kth.sda.skeleton.auth.AuthService;
+import se.kth.sda.skeleton.user.User;
+import se.kth.sda.skeleton.user.UserService;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostTests {
     @Autowired
     PostController postController;
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    AuthService authService;
+
     @MockBean
     PostService postService;
+
+    private String getAuthHeader() {
+
+        return "Bearer " + authService.createAuthToken("test@exmaple.com");
+    }
 
     @Test
     public void getById() {
@@ -26,12 +42,47 @@ public class PostTests {
         post.setDate("2020");
         Mockito.when(postService.getByID(Mockito.eq(1L)))
                 .thenReturn(Optional.of(post));
-
-
         // Act
         Post got = postController.getById(1L);
-
         // Assert
         Assertions.assertEquals(post.getName(), got.getName());
     }
+
+    @Test
+    public void getAllTest() {
+        // Arrange
+        Post post1 = new Post();
+        Post post2 = new Post();
+        Post post3 = new Post();
+        List<Post> postList = new ArrayList<Post>();
+        postList.add(post1);
+        postList.add(post2);
+        postList.add(post3);
+        Mockito.when(postService.getAll())
+                .thenReturn(postList);
+        // Act
+        List<Post> mockedList = postService.getAll();
+        // Assert
+        Assertions.assertEquals(postList, mockedList);
+    }
+
+    @Test
+    public void updatePostWithAtendeeInfoTest() throws Exception {
+//        // Arrange
+//        Post post = new Post();
+//        post.setId(1L);
+//
+//        User attendee = new User("test@exmaple.com", "pass123123", "John");
+//        post.setUser(attendee);
+//        postController.savePost(post);
+//        userService.register(attendee);
+//
+//        Mockito.when(postService.updatePostWithAttendeeInfo(Mockito.eq(post), Mockito.eq(attendee)))
+//                .thenReturn(post);
+//        // Act
+//        Post mockedUser = postController.updatePostWithAttendeeInfo(1L);
+//        // Assert
+//        Assertions.assertEquals(post.getName(), mockedUser.getName());
+    }
+
 }
