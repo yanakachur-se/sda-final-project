fb5c7b16e14d4da7c76eb1fea45cd4944acd6b1a
Maria Gubolin <mlgubolin@gmail.com>
2020-05-12 15:07:18 +0200

    first test working :DDDDDDDD

--- src/test/java/se/kth/sda/skeleton/comments/CommentTest.java
+++ src/test/java/se/kth/sda/skeleton/comments/CommentTest.java
@@ -1,9 +1,75 @@
 package se.kth.sda.skeleton.comments;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import se.kth.sda.skeleton.auth.AuthService;
+import se.kth.sda.skeleton.user.User;
+import se.kth.sda.skeleton.user.UserService;
 
 @SpringBootTest
 class ControllerTests {
-
+//    @Autowired
+//    TestRestTemplate testRestTemplate;
+//
+//    @Autowired
+//    AuthService authService;
+//
+//    @Autowired
+//    UserService userService;
+//
+//    private String getAuthHeader() {
+//        userService.register(new User("test@exmaple.com", "pass123123", "John"));
+//        return "Bearer " + authService.createAuthToken("test@exmaple.com");
+//    }
+//
+//    @Test
+//    public void getAll() {
+//        // Arrange
+//        // Add tasks
+//
+//        // Act
+//        // Send an http request to /tasks
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add(HttpHeaders.AUTHORIZATION, getAuthHeader());
+//        HttpEntity<String> request = new HttpEntity<>("", headers);
+//
+//        String body = testRestTemplate.exchange(
+//                "/tasks",
+//                HttpMethod.GET,
+//                request,
+//                String.class
+//        ).getBody();
+//
+//        // Assert
+//        Assertions.assertEquals("[]", body);
+//    }
+//
+//    @Test
+//    public void getAll1() {
+//        // Arrange
+//        // Add tasks
+//
+//        // Act
+//        // Send an http request to /tasks
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add(HttpHeaders.AUTHORIZATION, getAuthHeader());
+//        HttpEntity<String> request = new HttpEntity<>("", headers);
+//
+//        String body = testRestTemplate.exchange(
+//                "/tasks",
+//                HttpMethod.GET,
+//                request,
+//                String.class
+//        ).getBody();
+//
+//        // Assert
+//        Assertions.assertEquals("[]", body);
+//    }
 
 }
--- src/test/java/se/kth/sda/skeleton/posts/PostTests.java
+++ src/test/java/se/kth/sda/skeleton/posts/PostTests.java
@@ -1,6 +1,37 @@
 package se.kth.sda.skeleton.posts;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+@SpringBootTest
 public class PostTests {
+    @Autowired
+    PostController postController;
+
+    @MockBean
+    PostService postService;
+
+    @Test
+    public void getById() {
+        // Arrange
+        Post post = new Post();
+        post.setId(1L);
+        post.setName("Test name");
+        post.setDate("2020");
+        Mockito.when(postService.getByID(Mockito.eq(1L)))
+                .thenReturn(Optional.of(post));
+
+
+        // Act
+        Post got = postController.getById(1L);
+
+        // Assert
+        Assertions.assertEquals(post.getName(), got.getName());
+    }
 }
